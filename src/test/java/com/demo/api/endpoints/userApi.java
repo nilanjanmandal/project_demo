58f4c6a2b1336ba2843885eba531280b234abf45
Nilanjan mandal <nilanjanm@testvagrant.com>
2023-05-04 11:38:14 +0530

    update

---
+++ src/test/java/com/demo/api/endpoints/userApi.java
@@ -0,0 +1,12 @@
+package com.demo.api.endpoints;
+import  static io.restassured.RestAssured.*;
+
+public class userApi {
+
+    public static Response createUser(createUser payload){
+        Response response = given().
+
+
+    }
+
+}
